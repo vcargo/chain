4d4bb3c8077cf39a9f2a53ea3e11d4186a1cd515
vcargo <158460728+vcargo@users.noreply.github.com>
2024-07-05 03:42:06 -0700

    update pyth examples

--- examples/pyth/examples/price_feed.rs
+++ examples/pyth/examples/price_feed.rs
@@ -1,5 +1,5 @@
 use anyhow::anyhow;
-use chrono::{NaiveTime, Weekday};
+use chrono::{Datelike, Duration, Utc, Weekday};
 use chrono_tz::Tz;
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -17,8 +17,7 @@ pub struct WeeklySchedule {
     pub timezone: String,
     pub weekday: Weekday,
     pub mh_kind: MHKind,
-    pub start_time: NaiveTime,
-    pub end_time: NaiveTime,
+    pub open_time: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -35,59 +34,127 @@ impl PriceFeed {
         Ok(price_feeds)
     }
 
-    // https://docs.pyth.network/price-feeds/how-pyth-works/weekly-schedule-format
+    // https://docs.pyth.network/price-feeds/pythnet-reference/schedule-format
     pub async fn parse(&self) -> Result<Vec<WeeklySchedule>, anyhow::Error> {
-        let weekly_schedule = match self.attributes.get("weekly_schedule") {
-            Some(s) => s,
-            None => {
-                return Ok(vec![]);
+        if let Some(schedule) = self.attributes.get("schedule") {
+            let parts: Vec<&str> = schedule.split(';').collect();
+            if parts.len() != 3 {
+                return Err(anyhow!("invalid schedule: {}", schedule));
             }
-        };
 
-        let parts: Vec<&str> = weekly_schedule.split(",").collect();
-        if parts.len() != 8 {
-            return Err(anyhow!("invalid weekly_schedule: {}", weekly_schedule));
-        }
+            let timezone = parts[0].trim();
+            let tz: Tz = timezone.parse().map_err(|e| anyhow!("{}", e))?;
+            let date = Utc::now().with_timezone(&tz);
 
-        let timezone = parts[0].trim();
-        timezone
-            .parse::<Tz>()
-            .map_err(|e| anyhow!("timezone error: {:?}", e))?;
+            let mut holidays = HashMap::new();
+            for pair in parts[2].trim().split(',') {
+                let mut kv = pair.split('/');
+                if let (Some(key), Some(value)) = (kv.next(), kv.next()) {
+                    holidays.insert(key.to_string(), value.to_string());
+                }
+            }
 
-        let res: Result<Vec<WeeklySchedule>, Box<dyn Error>> = parts
-            .iter()
-            .enumerate()
-            .skip(1)
-            .map(|(i, &part)| {
-                let (mh_kind, start, end) = match part {
-                    "O" => (MHKind::Open, "00:00", "00:00"),
-                    "C" => (MHKind::Closed, "00:00", "00:00"),
-                    _ => {
-                        let (start, mut end) = part
-                            .split_once("-")
-                            .ok_or(format!("'{}' is not a valid time range", part))?;
-                        if end == "24:00" {
-                            end = "00:00";
-                        }
-                        (MHKind::Range, start, end)
+            let res: Result<Vec<WeeklySchedule>, Box<dyn Error>> = parts[1]
+                .trim()
+                .split(',')
+                .collect::<Vec<&str>>()
+                .iter()
+                .enumerate()
+                .map(|(i, &part)| {
+                    let mut part = part;
+                    let days = (7 - date.weekday().num_days_from_monday() + i as u32) % 7;
+                    let date = date + Duration::days(days as i64);
+                    let date = date.format("%m%d").to_string();
+                    if let Some(value) = holidays.get(&date) {
+                        part = value;
                     }
-                };
 
-                let w = i as u8;
+                    let (mh_kind, open_time) = match part {
+                        "O" => (MHKind::Open, "00:00-24:00".to_string()),
+                        "C" => (MHKind::Closed, "".to_string()),
+                        _ => {
+                            let parts: Vec<&str> = part.split('&').collect();
+                            let parts = parts
+                                .iter()
+                                .map(|&part| {
+                                    let ranges: Vec<&str> = part.split('-').collect();
+                                    format!(
+                                        "{}:{}-{}:{}",
+                                        &ranges[0][0..2],
+                                        &ranges[0][2..4],
+                                        &ranges[1][0..2],
+                                        &ranges[1][2..4]
+                                    )
+                                })
+                                .collect::<Vec<_>>()
+                                .join(",");
+                            (MHKind::Range, parts)
+                        }
+                    };
 
-                Ok(WeeklySchedule {
-                    timezone: timezone.to_string(),
-                    weekday: Weekday::try_from(w % 7)?,
-                    mh_kind,
-                    start_time: NaiveTime::parse_from_str(start, "%H:%M")?,
-                    end_time: NaiveTime::parse_from_str(end, "%H:%M")?,
+                    Ok(WeeklySchedule {
+                        timezone: timezone.to_string(),
+                        weekday: Weekday::try_from((i as u8 + 1) % 7)?,
+                        mh_kind,
+                        open_time,
+                    })
                 })
-            })
-            .collect();
+                .collect();
+            match res {
+                Ok(sessions) => Ok(sessions),
+                Err(e) => Err(anyhow!("{}", e)),
+            }
+        } else if let Some(weekly_schedule) = self.attributes.get("weekly_schedule") {
+            let parts: Vec<&str> = weekly_schedule.split(',').collect();
+            if parts.len() != 8 {
+                return Err(anyhow!("invalid weekly_schedule: {}", weekly_schedule));
+            }
 
-        match res {
-            Ok(sessions) => Ok(sessions),
-            Err(e) => Err(anyhow!("{}", e)),
+            let timezone = parts[0].trim();
+
+            let res: Result<Vec<WeeklySchedule>, Box<dyn Error>> = parts
+                .iter()
+                .enumerate()
+                .skip(1)
+                .map(|(i, &part)| {
+                    let (mh_kind, open_time) = match part {
+                        "O" => (MHKind::Open, "00:00-24:00"),
+                        "C" => (MHKind::Closed, ""),
+                        _ => (MHKind::Range, part),
+                    };
+
+                    let w = i as u8;
+
+                    Ok(WeeklySchedule {
+                        timezone: timezone.to_string(),
+                        weekday: Weekday::try_from(w % 7)?,
+                        mh_kind,
+                        open_time: open_time.to_string(),
+                    })
+                })
+                .collect();
+            match res {
+                Ok(sessions) => Ok(sessions),
+                Err(e) => Err(anyhow!("{}", e)),
+            }
+        } else {
+            let timezone = "America/New_York";
+            let open_time = "00:00-24:00";
+            let res: Result<Vec<WeeklySchedule>, Box<dyn Error>> = (0..7)
+                .map(|w| {
+                    let weekday = Weekday::try_from((w + 1) % 7)?;
+                    Ok(WeeklySchedule {
+                        timezone: timezone.to_string(),
+                        weekday,
+                        mh_kind: MHKind::Open,
+                        open_time: open_time.to_string(),
+                    })
+                })
+                .collect();
+            match res {
+                Ok(sessions) => Ok(sessions),
+                Err(e) => Err(anyhow!("{}", e)),
+            }
         }
     }
 }
--- examples/pyth/examples/symbol_info.rs
+++ examples/pyth/examples/symbol_info.rs
@@ -58,6 +58,7 @@ enum Group {
     Forex,
     Crypto,
     CFD,
+    Bond,
 }
 
 #[tokio::main]
